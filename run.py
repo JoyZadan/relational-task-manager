2ac190fe32b416872c12873fa30b1c45465a235c
JoyZadan <joyzadan.learning@gmail.com>
2022-05-16 17:46:44 +0000

    Create the main python file, run.py, at the root level, add imports, tell app how and where to run the application, create templates folder and base.html file

---
+++ run.py
@@ -0,0 +1,14 @@
+"""
+imports os
+and imports app variable from taskmanager folder
+"""
+import os
+from taskmanager import app
+
+
+if __name__ == "__main__":
+    app.run(
+        host=os.environ.get("IP"),
+        port=int(os.environ.get("PORT")),
+        debug=os.environ.get("DEBUG")
+    )
