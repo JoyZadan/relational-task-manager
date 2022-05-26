6494c12bbf722d898d2979c4074b8b6467ff1019
JoyZadan <joyzadan.learning@gmail.com>
2022-05-26 11:42:12 +0000

    Feat: add the Update functionality for the categories, edit categories cards for better contrast

--- taskmanager/static/js/script.js
+++ taskmanager/static/js/script.js
@@ -11,7 +11,7 @@ const nav = document.querySelector('.nav');
 window.onscroll = function() {
   var top = window.scrollY;
   console.log(top);
-  if (top >= 100) {
+  if (top >= 50) {
     nav.classList.add('active');
   } else {
     nav.classList.remove('active');
