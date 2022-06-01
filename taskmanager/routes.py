6d9181e2d758114fc58bf02b684b60cb0a8943a5
JoyZadan <joyzadan.learning@gmail.com>
2022-06-01 20:34:24 +0000

    Feat: add edit_task functionality and render template for user to edit tasks

--- taskmanager/routes.py
+++ taskmanager/routes.py
@@ -16,6 +16,7 @@ def home():
     this will be used to target the function, 'home'
     the function 'home' will return the rendered-template
     of 'base.html'
+    function to display existing taks in the database
     """
     tasks = list(Task.query.order_by(Task.id).all())
     return render_template("tasks.html", tasks=tasks)
@@ -89,4 +90,22 @@ def add_task():
         db.session.commit()
         return redirect(url_for("home"))
     return render_template("add_task.html", categories=categories)
+
+
+@app.route("/edit_task/<int:task_id>", methods=["GET", "POST"])
+def edit_task(task_id):
+    """
+    get the template and render it
+    """
+    task = Task.query.get_or_404(task_id)
+    categories = list(Category.query.order_by(Category.category_name).all())
+    if request.method == "POST":
+        task.task_name = request.form.get("task_name")
+        task.task_description = request.form.get("task_description")
+        task.is_urgent = bool(True if request.form.get("is_urgent") else False)
+        task.due_date = request.form.get("due_date")
+        task.category_id = request.form.get("category_id")
+        db.session.commit()
+    return render_template("edit_task.html", task=task, categories=categories)
+
 
