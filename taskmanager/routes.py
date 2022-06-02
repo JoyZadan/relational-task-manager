3e870a2c6d52eb8fe153af06a01ebda39c8c672d
JoyZadan <joyzadan.learning@gmail.com>
2022-06-02 09:53:22 +0000

    Feat: add delete_task functionality with modal popup to warn users before deleting task

--- taskmanager/routes.py
+++ taskmanager/routes.py
@@ -109,3 +109,12 @@ def edit_task(task_id):
     return render_template("edit_task.html", task=task, categories=categories)
 
 
+@app.route("/delete_task/<int:task_id>")
+def delete_task(task_id):
+    """
+    get the template and render it
+    """
+    task = Task.query.get_or_404(task_id)
+    db.session.delete(task)
+    db.session.commit()
+    return redirect(url_for("home"))
